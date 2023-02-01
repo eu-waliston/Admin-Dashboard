f4e201964f75980002ad4c495a8476c08d9c0e16
walwizard <goneisgone@hotmail.com>
2023-01-31 23:36:23 -0300

    Admin Dashboard TopBar Finalizada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
 
-import Sidebar from './components/sidebar/Sidebar'; 
+import Sidebar from './components/sidebar/Sidebar';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
+      <Topbar />
       <Sidebar />
     </div>
   );
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,15 +1,35 @@
 import React from "react";
 import './Topbar.css';
 
+
 const Topbar = () => {
+
+    const [seed, setSeed] = React.useState("");
+
+    React.useEffect(() => {
+      setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
         <div className="top__bar">
-            <input type={"text"} placeholder={"Search for..."}/>
-            <i class="bi bi-search input__inner__icon"></i>
-            <i class="bi bi-envelope"></i>
-            <i class="bi bi-chat"></i>
-            <i class="bi bi-person-circle"></i>
-            <i class="bi bi-caret-down-fill"></i>
+            <img src={require("../assets/logo.png")} className="logo__topbar" alt={"logo"} />
+            <div id="itens">
+
+                <input type={"text"} placeholder={"  Search for..."} className={"input__side"} />
+                <i className="bi bi-search input__inner__icon icons"></i>
+                <i className="bi bi-envelope icons"></i>
+                <i className="bi bi-chat icons"></i>
+                <img src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} alt="profile" className="profile__photo"/>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="/">Profile</a></li>
+                        <li><a className="dropdown-item" href="/">Log out</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
