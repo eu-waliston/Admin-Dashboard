7d0cd73765ac693afafb557448b14c08015bcff0
walwizard <goneisgone@hotmail.com>
2023-01-31 23:58:44 -0300

    Admin Dashboard TopBar Finalizada :)

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -4,12 +4,6 @@ import './Topbar.css';
 
 const Topbar = () => {
 
-    const [seed, setSeed] = React.useState("");
-
-    React.useEffect(() => {
-      setSeed(Math.floor(Math.random() * 5000));
-    }, []);
-
     return (
         <div className="top__bar">
             <img src={require("../assets/logo.png")} className="logo__topbar" alt={"logo"} />
@@ -19,7 +13,7 @@ const Topbar = () => {
                 <i className="bi bi-search input__inner__icon icons"></i>
                 <i className="bi bi-envelope icons"></i>
                 <i className="bi bi-chat icons"></i>
-                <img src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} alt="profile" className="profile__photo"/>
+                <img src={(require("../assets/pf.png"))} alt="profile" className="profile__photo"/>
                 <div className="dropdown">
                     <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 
