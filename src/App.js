c97bd3679e9e6df3ee275a250120df49c33a5a4e
walwizard <goneisgone@hotmail.com>
2023-02-02 03:20:30 -0300

    Dashboard Login Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css'
+import { Router, Route } from 'react-router-dom';
 
 import Sidebar from './components/navbar/sidebar/Sidebar'
-
 import Topbar from './components/navbar/topbar/Topbar'
-
 import Dashboard from './components/dashboard/Dashboard';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
-      <Sidebar />
-      <Dashboard />
+      <Router>
+        <Route exact path={"/"} element={<Login />} />
+        <Route exact path={"/app"} element={[<Topbar />, <Sidebar />, <Dashboard />]} />
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className="form__control">
+
+        </div>
+    )
+}
+
+export default Login;
